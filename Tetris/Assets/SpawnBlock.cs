efcb1d8a7344f6be95a5ec55e64991122e3f7f79
Nikita Bakun <not-only-yours@users.noreply.github.com>
2020-03-21 15:34:46 +0200

    Added All Playing mode (fixed bugs)

---
+++ Tetris/Assets/SpawnBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlock : MonoBehaviour
+{
+    public GameObject[] Blocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewBlock();
+    }
+
+    
+    public void NewBlock()
+    {
+        Instantiate(Blocks[Random.Range(0, Blocks.Length)], transform.position, Quaternion.identity);
+    }
+}
--- Tetris/Assets/TetrisBlock.cs
+++ Tetris/Assets/TetrisBlock.cs
@@ -9,6 +9,7 @@ public class TetrisBlock : MonoBehaviour
     public float fallTime = 0.8f;
     public static int height = 20;
     public static int wight = 10;
+    private static Transform[,] grid = new Transform[wight, height];
     // Start is called before the first frame update
     void Start()
     {
@@ -43,21 +44,87 @@ public class TetrisBlock : MonoBehaviour
         {
                 transform.position += new Vector3(0, -1, 0);
             if (!ValidMove())
+            {
                 transform.position += new Vector3(0, 1, 0);
+                AddToGrid();
+                CheckLines();
+                this.enabled = false;
+                FindObjectOfType<SpawnBlock>().NewBlock();
+            }
             previousTime = Time.time;
+        }
+    }
+
+    void CheckLines()
+    {
+        for(int i=height-1;i>=0;i--)
+        {
+            if(HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    bool HasLine(int i)
+    {
+        for(int j=0;j<wight;j++)
+        {
+            if (grid[j, i] == null)
+                return false;
+        }
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < wight; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int j = 0; j < wight; j++)
+            {
+                if (grid[j, y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
         }
     }
 
     bool ValidMove()
     {
-        foreach(Transform children in transform)
+        foreach (Transform children in transform)
         {
             int roundedX = Mathf.RoundToInt(children.transform.position.x);
             int roundedY = Mathf.RoundToInt(children.transform.position.y);
-            if(roundedX < 0 || roundedX >= wight || roundedY < 0 || roundedY >= height)
+            if (roundedX < 0 || roundedX >= wight || roundedY < 0 || roundedY >= height)
             {
                 return false;
             }
+            if (grid[roundedX, roundedY] != null)
+                return false;
         }
         return true;
     }
